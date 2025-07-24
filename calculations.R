bdb6c7f3fa31d98ce4b5a02dd0937171463bb8b0
StatWiseAI <info@statwiseai.com>
2025-07-24 18:45:01 +0200

    added new files
    added new files

---
+++ calculations.R
@@ -0,0 +1,8 @@
+# Financial calculations
+calculate_returns <- function(prices) {
+  # Your calculation logic
+}
+
+calculate_risk_metrics <- function(returns) {
+  # Your risk calculation logic
+}
---
+++ data_processing.R
@@ -0,0 +1,8 @@
+# Data processing functions
+load_financial_data <- function(file_path) {
+  # Your data loading logic
+}
+
+clean_financial_data <- function(data) {
+  # Your data cleaning logic
+}
